462551a556d5bec5301d5705f716ca3ae75c5fdb
Krish Verma <dhurandhar0987@gmail.com>
2024-07-02 22:26:29 +0530

    Implemented CategoryActivity i.e. showing related recipes on clicking any category.

--- app/src/main/java/com/example/cookbook/HomeActivity.kt
+++ app/src/main/java/com/example/cookbook/HomeActivity.kt
@@ -18,7 +18,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding=ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -27,28 +27,55 @@ class HomeActivity : AppCompatActivity() {
             insets
         }
 
+        binding.salad.setOnClickListener {
+            val myIntent = Intent(this@HomeActivity, CategoryActivity::class.java)
+            myIntent.putExtra("TITTLE","Salad")
+            myIntent.putExtra("CATEGORY","Salad")
+            startActivity(myIntent)
+        }
+        binding.mainDish.setOnClickListener {
+            val myIntent = Intent(this@HomeActivity, CategoryActivity::class.java)
+            myIntent.putExtra("TITTLE","Main Dish")
+            myIntent.putExtra("CATEGORY","Dish")
+            startActivity(myIntent)
+        }
+        binding.drinks.setOnClickListener {
+            val myIntent = Intent(this@HomeActivity, CategoryActivity::class.java)
+            myIntent.putExtra("TITTLE","Drinks")
+            myIntent.putExtra("CATEGORY","Drinks")
+            startActivity(myIntent)
+        }
+        binding.desserts.setOnClickListener {
+            val myIntent = Intent(this@HomeActivity, CategoryActivity::class.java)
+            myIntent.putExtra("TITTLE","Desserts")
+            myIntent.putExtra("CATEGORY","Desserts")
+            startActivity(myIntent)
+        }
+
         setUpRecyclerView()
         binding.search.setOnClickListener {
-            startActivity(Intent(this,SearchActivity::class.java))
+            startActivity(Intent(this, SearchActivity::class.java))
+
         }
     }
 
     private fun setUpRecyclerView() {
-        dataList= ArrayList()
-        binding.rvPopular.layoutManager= LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        var db=databaseBuilder(this@HomeActivity,AppDatabase::class.java,"db_name")
+        dataList = ArrayList()
+        binding.rvPopular.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        var db = databaseBuilder(this@HomeActivity, AppDatabase::class.java, "db_name")
             .allowMainThreadQueries()
             .fallbackToDestructiveMigration()
             .createFromAsset("recipe.db")
             .build()
-        var daoObject=db.getDao()
-        var recipes =daoObject.getAll()
-        for(i in recipes!!.indices){
-            if(recipes[i]!!.category.contains("Popular")){
+        var daoObject = db.getDao()
+        var recipes = daoObject.getAll()
+        for (i in recipes!!.indices) {
+            if (recipes[i]!!.category.contains("Popular")) {
                 dataList.add(recipes[i]!!)
             }
-            rvAdapter=PopularAdapter(dataList,this)
-            binding.rvPopular.adapter=rvAdapter
+            rvAdapter = PopularAdapter(dataList, this)
+            binding.rvPopular.adapter = rvAdapter
         }
     }
 }
